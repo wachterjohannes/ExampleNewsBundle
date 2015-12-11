65798814c8d1f0f02368e9dd6be48275f1e28efc
Johannes Wachter <johannes.wachter@massiveart.at>
2015-12-11 11:59:25 +0100

    added change detection to enable save button

--- Resources/public/js/components/news/form/main.js
+++ Resources/public/js/components/news/form/main.js
@@ -37,10 +37,20 @@ define(['text!./form.html'], function(form) {
 
         initialize: function() {
             this.render();
+
+            this.bindDomEvents();
         },
 
         render: function() {
             this.$el.html(this.templates.form({translations: this.translations}));
+
+            this.sandbox.form.create('#news-form');
+        },
+
+        bindDomEvents: function() {
+            this.$el.find('input, textarea').on('keypress', function() {
+                this.sandbox.emit('sulu.header.toolbar.item.enable', 'save');
+            }.bind(this));
         }
     };
 });
