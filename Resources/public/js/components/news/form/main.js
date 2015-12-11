8e535c22b5579575566661c399b048b21b1842da
Johannes Wachter <johannes.wachter@massiveart.at>
2015-12-11 10:52:50 +0100

    added header and layout options

--- Resources/public/js/components/news/form/main.js
+++ Resources/public/js/components/news/form/main.js
@@ -1,5 +1,27 @@
 define(function() {
     return {
+
+        header: {
+            title: function() {
+                return 'news.headline';
+            }.bind(this),
+            toolbar: {
+                buttons: {
+                    save: {
+                        parent: 'saveWithOptions'
+                    }
+                }
+            }
+        },
+
+        layout: {
+            content: {
+                width: 'fixed',
+                leftSpace: true,
+                rightSpace: true
+            }
+        },
+
         initialize: function() {
             this.$el.html('<h1>Hello awesome sulu world</h1>');
         }
